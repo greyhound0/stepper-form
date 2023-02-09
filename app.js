58341e10c168fa3e368ddfd4200ab2b353ac1f9c
manas gupta <manasgupta0110@gmail.com>
2023-02-09 17:37:44 +0530

    my

---
+++ app.js
@@ -0,0 +1,3 @@
+const firstStep = document.getElementsByClassName("firstStep");
+const secondStep = document.getElementsByClassName("secondStep");
+const thirdStep = document.getElementsByClassName("thirdStep");
