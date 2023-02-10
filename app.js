e9aa955366ba0644b621e0cb19b3b90e32ea0eb7
manas gupta <manasgupta0110@gmail.com>
2023-02-11 02:54:40 +0530

    my

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const address = document.getElementById("address");
 const facebook = document.getElementById("facebook");
 const twitter = document.getElementById("twitter");
 const error = document.getElementById("error");
+const error1 = document.getElementById("error1");
+const error2 = document.getElementById("error2");
 const next = document.querySelector(".next")
 const previous = document.querySelector('.previous')
 const next1 = document.querySelector('.next1')
@@ -21,7 +23,6 @@ const submit = document.querySelector('.submit')
 function validateEmail(){
     const valid = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if(!valid.test(email.value)){
-        // console.log(valid.test(email.value))
          error.style.display = "block";
          
                  
@@ -32,6 +33,7 @@ function validateEmail(){
         localStorage.setItem('password', password.value);
             firstStep.item(item=>item).classList.add('inactive');
             secondStep.item(item=>item).classList.add('active');
+            error.style.display = 'none'
 
         }
         else {
@@ -44,7 +46,6 @@ function validateEmail(){
  
 next.addEventListener('click', validateEmail)
 previous.addEventListener('click', () => {
-    console.log('@@@')
     secondStep.item(item=>item).classList.add('inactive');
     secondStep.item(item=>item).classList.remove('active');
     firstStep.item(item=>item).classList.add('active');
@@ -59,10 +60,16 @@ function personalDetails(){
     secondStep.item(item=>item).classList.add('inactive');
     secondStep.item(item=>item).classList.remove('active');
     thirdStep.item(item=>item).classList.add('active');
+    error1.style.display =  'none';
+
+    }
+    else if(phone.value.length != 10){
+        error1.innerText = 'enter a valid phone no.';
+        error1.style.display = 'block'
     }
     else {
-        error.innerText = 'fields cannot be empty';
-        error.style.display = 'block';
+        error1.innerText = 'fields cannot be empty';
+        error1.style.display = 'block';
     }
 }
 
@@ -71,14 +78,21 @@ previous1.addEventListener('click', ()=> {
     thirdStep.item(item=>item).classList.add('inactive');
     thirdStep.item(item=>item).classList.remove('active');
     secondStep.item(item=>item).classList.add('active');
+    error1.style.display = 'none';
 })
 
 submit.addEventListener('click', ()=>{
     if(facebook.value.length >0 && twitter.value.length >0 ){
+        error2.style.display = 'none'
         generateDataTable();
     }
+    else{
+        error2.style.display = 'block';
+    }
 })
+window.addEventListener('load', localStorage.clear)
 
 function generateDataTable(){
-
+    thirdStep.item(item=>item).classList.add('inactive');
+    thirdStep.item(item=>item).classList.remove('active');
 }
